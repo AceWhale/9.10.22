654ae64b9ffeec396136086c0db4af4fb0fb35b8
Whale <mohseninikita9@gmail.com>
2022-10-01 10:59:24 +0300

    Add project files.

---
+++ Бинарное дерево/Main.cpp
@@ -0,0 +1,53 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include "Functions.h"
+#include "Tree.h"
+#include "Windows.h"
+
+static int a;
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+	cout << a;
+
+    Tree tree;
+    bool exit = false;
+
+    for (;;)
+	{
+		PrintMenu();
+
+		int choice = _getch();
+
+		switch (choice)
+		{
+			case '1': 
+                Insert(tree);
+                break;
+			case '2': 
+                Remove(tree);
+                break;
+			case '3': 
+                Print(tree);
+                break;
+			case '4': 
+                Search(tree);
+                break;
+            case '5':
+                exit = true;
+                break;
+		}
+
+        if (exit)
+        {
+            break;
+        }
+	}
+
+    return 0;
+}
---
+++ Бинарное дерево/Node.cpp
@@ -0,0 +1,12 @@
+﻿#include "Node.h"
+#include <fstream>
+
+char* Node::GetEnglish()
+{
+    return m_english;
+}
+
+char* Node::GetRussian()
+{
+    return m_russian;
+}
